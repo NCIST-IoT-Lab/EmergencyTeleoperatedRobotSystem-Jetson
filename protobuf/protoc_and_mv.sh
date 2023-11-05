8a9e9cd29719477612e82a19a817e82b27e390bd
Jetson Xavier NX <2670226747@qq.com>
2023-11-05 23:13:05 +0800

    fix: 修复protoc路径错误

--- protobuf/protoc_and_mv.sh
+++ protobuf/protoc_and_mv.sh
@@ -8,7 +8,7 @@ SCRIPT_PATH=$(cd "$(dirname "$0")"; pwd)
 PROTO_PATH=$SCRIPT_PATH
 
 CPP_PATH=$SCRIPT_PATH/../build
-PYTHON_PATH=$SCRIPT_PATH/../src/python/protobuf
+PYTHON_PATH=$SCRIPT_PATH/../src/object_detection/protobuf
 CSHARP_PATH=$SCRIPT_PATH/csharp
 
 protoc -I=$PROTO_PATH --cpp_out=$CPP_PATH --python_out=$PYTHON_PATH --csharp_out=$CSHARP_PATH $PROTO_PATH/*.proto 
