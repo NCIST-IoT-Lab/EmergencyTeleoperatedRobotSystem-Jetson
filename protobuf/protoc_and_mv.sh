dd9c804322aaa8b416a895639b8241989fb90fc8
Jetson Xavier NX <2670226747@qq.com>
2023-11-12 15:17:37 +0800

    rmfeat: 删除目标检测模块的protobuf

--- protobuf/protoc_and_mv.sh
+++ protobuf/protoc_and_mv.sh
@@ -8,10 +8,11 @@ SCRIPT_PATH=$(cd "$(dirname "$0")"; pwd)
 PROTO_PATH=$SCRIPT_PATH
 
 CPP_PATH=$SCRIPT_PATH/../build
-PYTHON_PATH=$SCRIPT_PATH/../src/object_detection/protobuf
+# PYTHON_PATH=$SCRIPT_PATH/../src/object_detection/protobuf
 CSHARP_PATH=$SCRIPT_PATH/csharp
 
-protoc -I=$PROTO_PATH --cpp_out=$CPP_PATH --mypy_out=$PYTHON_PATH --python_out=$PYTHON_PATH --csharp_out=$CSHARP_PATH $PROTO_PATH/*.proto 
+# protoc -I=$PROTO_PATH --cpp_out=$CPP_PATH --mypy_out=$PYTHON_PATH --python_out=$PYTHON_PATH --csharp_out=$CSHARP_PATH $PROTO_PATH/*.proto 
+protoc -I=$PROTO_PATH --cpp_out=$CPP_PATH --csharp_out=$CSHARP_PATH $PROTO_PATH/*.proto 
 
 # 压缩 C# 文件
 cd $CSHARP_PATH
